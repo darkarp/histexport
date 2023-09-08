70ac0a74fc4881c6708b6f0a03f11b68a7274857
Mário Nascimento <mario@whitehathacking.tech>
2023-09-08 16:25:28 +0100

    renaming modules and fixing readme

--- histexport/histexport.py
+++ histexport/histexport.py
@@ -1,3 +1,4 @@
+import os
 import sqlite3
 import pandas as pd
 import argparse
@@ -22,35 +23,30 @@ def connect_db(path_to_history):
 
 
 def pretty_txt(df, file_name):
-    # Find the longest field name to properly align all field names
     longest_field_name = max(df.columns, key=len)
     field_name_length = len(longest_field_name)
 
-    # Calculate the length of the longest value to align all '='
-    max_length = 0
-    for _, row in df.iterrows():
-        for field, value in zip(df.columns, row):
-            length = len(f"{field}: {value}")
-            if length > max_length:
-                max_length = length
+    max_length = max(
+        len(f"{field}: {value}")
+        for _, row in df.iterrows()
+        for field, value in zip(df.columns, row)
+    )
 
     with open(file_name, 'w', encoding='utf-8') as f:
         for _, row in df.iterrows():
             for field, value in zip(df.columns, row):
-                # Left-align the field names and right-align the values
                 f.write(f"{field.ljust(field_name_length)}: {value}\n")
             f.write("=" * max_length + "\n")
 
 
-def fetch_and_write_data(conn, output_base, formats, extract_types):
+def fetch_and_write_data(conn, output_dir, output_base, formats, extract_types):
     cursor = conn.cursor()
     epoch_start = datetime(1601, 1, 1)
 
     def convert_chrome_time(chrome_time):
         return epoch_start + timedelta(microseconds=chrome_time)
 
-    def fetch_and_convert_data(conn, query, columns, time_cols):
-        cursor = conn.cursor()
+    def fetch_and_convert_data(query, columns, time_cols):
         cursor.execute(query)
         rows = cursor.fetchall()
         df = pd.DataFrame(rows, columns=columns)
@@ -61,15 +57,16 @@ def fetch_and_write_data(conn, output_base, formats, extract_types):
 
     query_dict = {
         'urls': ("SELECT url, title, visit_count, last_visit_time FROM urls", ['URL', 'Title', 'Visit_Count', 'Last_Visit_Time'], ['Last_Visit_Time']),
-        'downloads': ("""SELECT downloads.target_path, downloads.start_time, downloads.end_time, downloads.total_bytes, downloads.received_bytes, downloads_url_chains.url FROM downloads INNER JOIN downloads_url_chains ON downloads.id=downloads_url_chains.id""",
+        'downloads': ("SELECT downloads.target_path, downloads.start_time, downloads.end_time, downloads.total_bytes, downloads.received_bytes, downloads_url_chains.url FROM downloads INNER JOIN downloads_url_chains ON downloads.id=downloads_url_chains.id",
                       ['Target_Path', 'Start_Time', 'End_Time', 'Total_Bytes', 'Received_Bytes', 'URL'], ['Start_Time', 'End_Time'])
     }
 
     for extract_type in extract_types:
         query, columns, time_cols = query_dict[extract_type]
-        df = fetch_and_convert_data(conn, query, columns, time_cols)
+        df = fetch_and_convert_data(query, columns, time_cols)
         for fmt in formats:
-            output_file = f"{output_base}_{extract_type}.{fmt}"
+            output_file = os.path.join(
+                output_dir, f"{output_base}_{extract_type}.{fmt}")
             if fmt == 'csv':
                 df.to_csv(output_file, index=False)
             elif fmt == 'xlsx':
@@ -81,25 +78,39 @@ def fetch_and_write_data(conn, output_base, formats, extract_types):
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Export Chromium-based browser and download history to various formats.")
+        description="Export Chromium-based browser and download history to various formats.",
+        formatter_class=argparse.RawTextHelpFormatter,
+        epilog="""Examples:
+        1) Extract URLs and save to CSV and TXT:
+            histexport -i 'path/to/history' -o 'output' -f csv txt -e urls
+        2) Extract downloads and save to XLSX:
+            histexport -i 'path/to/history' -o 'output' -f xlsx -e downloads
+        3) Enable logging and specify output directory:
+            histexport -i 'path/to/history' -o 'output' -d '/output/dir' -l
+        """)
     parser.add_argument('-i', '--input', required=True,
                         help='Path to the SQLite history file.')
     parser.add_argument('-o', '--output', required=True,
                         help='Base name for the output files.')
+    parser.add_argument('-d', '--dir', required=False, default='./',
+                        help='Output directory. Default is current directory')
     parser.add_argument(
-        '-f', '--formats', nargs='+', choices=['csv', 'xlsx', 'txt'],
-        default=['csv'],
-        help='Output formats. Multiple formats can be specified.')
+        '-f', '--formats', nargs='+', choices=['csv', 'xlsx', 'txt'], default=['txt'],
+        help='Output formats. Multiple formats can be specified. Default is txt')
     parser.add_argument('-e', '--extract', nargs='+', choices=['urls', 'downloads'], default=[
-                        'urls', 'downloads'], help='Types to extract: urls, downloads, or both.')
+                        'urls', 'downloads'], help='Types to extract: urls, downloads, or both. Default is both')
     parser.add_argument('-l', '--log', action='store_true',
-                        help='Enable logging.')
+                        help='Enable logging. Default is disabled')
 
     args = parser.parse_args()
     init_logging(args.log)
 
+    if not os.path.exists(args.dir):
+        os.makedirs(args.dir)
+
     conn = connect_db(args.input)
-    fetch_and_write_data(conn, args.output, args.formats, args.extract)
+    fetch_and_write_data(conn, args.dir, args.output,
+                         args.formats, args.extract)
 
 
 if __name__ == "__main__":
